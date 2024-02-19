b243c2ca0cad8aebee8ac2b5014c67457a6645c9
Kryss <kryssbm99@gmail.com>
2024-02-19 14:31:38 -0400

    using puts instead of printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - main function void
+ *
+ *Description: This program uses printf to print typed message
+ *
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
