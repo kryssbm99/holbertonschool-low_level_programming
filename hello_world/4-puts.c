e4f93f1a9eed95d8aaec832c5384eaaf597a67d6
Kryss <kryssbm99@gmail.com>
2024-02-19 14:12:39 -0400

    using puts instead of printf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+/**
+ *main - main function
+ *
+ *Description: This program uses const char to make it as read only
+ *Then we use the message variable with puts to print the message
+ *
+ *Return: Always returns 0
+ */
 int main(void)
+
 {
 const char *message = "\"Programming is like building a multilingual puzzle";
 
