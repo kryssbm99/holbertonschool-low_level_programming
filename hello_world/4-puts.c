2983ad62cbe9a965fa8640911e0e61cee98220a6
Kryss <kryssbm99@gmail.com>
2024-02-19 13:44:29 -0400

    using puts instead of printf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-const char *message = "Programming is like building a multilingual puzzle\n"
+const char *message = "Programming is like building a multilingual puzzle\n";
 
 	   puts(message);
 
-return (0);
+return(0);
 }
