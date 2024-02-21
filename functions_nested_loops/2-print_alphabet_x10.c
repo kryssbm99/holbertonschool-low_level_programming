06737b4501887b21d5b3f9e5a08e4d899921f5b1
Kryss <kryssbm99@gmail.com>
2024-02-21 17:05:26 -0400

    Aplhabet goes hard

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print_alphabet_x10 function
+ *
+ *Description: Using a while loop and the prototypes to print the alphabet
+ *
+ *Return: Will be void
+ */
+void print_alphabet_x10(void)
+{
+char abc = 'a';
+int outer = 0;
+int inner = 0;
+	while (outer < 10)
+{
+		while (abc <= 'z')
+{
+		_putchar(abc);
+
+		abc++;
+}
+		_putchar('\n');
+		outer++;
+}
+}
