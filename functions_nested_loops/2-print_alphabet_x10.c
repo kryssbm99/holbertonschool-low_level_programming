1b59b71d47ba768a1d562b07256c46ad57f8b087
Kryss <kryssbm99@gmail.com>
2024-02-21 17:40:57 -0400

    Finally understood how the alphabet works

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,27 +2,24 @@
 /**
  *print_alphabet_x10 - print_alphabet_x10 function
  *
- *Description: Using a while loop and the prototypes to print the alphabet
- *
+ *Description: Using a for loop and the prototypes to print the alphabet
+ *10 times
  *Return: Will be void
  */
 void print_alphabet_x10(void)
 {
 char abc;
-int outer = 0;
-int inner = 0;
-	while (outer < 10)
+int x;
+
+for (x = 0; x < 10; x++)
 {
-	inner = 0;
-	abc = 'a';
-	while (inner < 26)
-{
-	_putchar(abc);
-	abc++;
-	inner++;
+	for (abc = 'a'; abc <= 'z'; abc ++)
+	{
+		_putchar(abc);
 }
-	_putchar('\n');
-	outer++;
-}
+		_putchar('\n');
+
+	}
+
 
 }
