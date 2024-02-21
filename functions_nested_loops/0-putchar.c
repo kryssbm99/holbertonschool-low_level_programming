e64296158d07b6e4ad2198d1d6fc8605aff58e4b
Kryss <kryssbm99@gmail.com>
2024-02-21 13:32:40 -0400

    Printing _putchar gives trouble!

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - main function
+ *
+ *Description:The program uses printf to print _putchar followed by a new line
+ *
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Welcome to my functions and nested loops basic training!
