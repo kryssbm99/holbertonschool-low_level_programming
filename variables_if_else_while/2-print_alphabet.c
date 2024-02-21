d1c43ff425d5c780b60a2653537f2866c1414a6f
Kryss <kryssbm99@gmail.com>
2024-02-21 10:10:37 -0400

    printing the aplhabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - main function
+ *
+ *Description:The program does this and that
+ *
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+	for (char abc = 'a'; abc <= 'z'; abc++)
+	{
+		putchar(abc);
+	}
+		putchar('\n');
+
+	return (0);
+}
