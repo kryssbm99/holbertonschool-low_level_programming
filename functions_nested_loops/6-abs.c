b64cf9ed290f9dd41dc5629ca9316ccf5a68bc3f
Kryss <kryssbm99@gmail.com>
2024-02-22 14:27:28 -0400

    My mind is ablaze

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_abs - Program checks for absolute value
+ *
+ *@r: character that is being checked
+ *
+ *Return: Returns 0
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+	r = -r;
+	return (r);
+	}
+		else
+		{
+		return (r);
+		}
+			return (0);
+}
