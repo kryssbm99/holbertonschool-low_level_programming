09ad123b6a93a97d033431e1a633a9a4ae530790
Kryss <kryssbm99@gmail.com>
2024-02-22 14:01:39 -0400

    Little by little getting better

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_sign - Program checks for lowercase
+ *
+ *@n: character that is being checked
+ *
+ *Return: This program returns 1,0 or -1 depending on the value of n
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		{
+		return (1);
+		_putchar('+');
+		}
+	else if (n == 0)
+		{
+		return (0);
+		_putchar('0');
+		}
+	else if (n < 0)
+		{
+		return (-1);
+		_putchar('-');
+		}
+
+	return (0);
+}
