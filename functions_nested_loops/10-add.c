0b59b490f9ee47128d2c89fec9eae3e27c002cac
Kryss <kryssbm99@gmail.com>
2024-02-23 00:02:50 -0400

    Late night coding 10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - Program adds two int
+ *
+ *@r: first character that is being checked
+ *@n: second character that is being checked
+ *Return: Returns sum of both int
+ */
+int add(int r, int n)
+{
+int x;
+x = r + n;
+return (x);
+}
