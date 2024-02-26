219830a4cb2468910be34693b436db9ef8cc72a9
Kryss <kryssbm99@gmail.com>
2024-02-26 13:16:03 -0400

    777 is the way to go diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_line - Program prints _ based on the value of n
+ *@n: variable for print_line
+ *Return: Always returns 0
+ */
+void print_line(int n)
+{
+int x;
+int y;
+	if (n > 0)
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				if (x != 0)
+				{
+				_putchar(' ');
+				}
+			}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+}
+}
