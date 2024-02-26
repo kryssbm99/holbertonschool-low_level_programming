6947b736f8125f32ce7f61d7e29b3ac62342b0e9
Kryss <kryssbm99@gmail.com>
2024-02-26 10:52:12 -0400

    WEEK 4 THIRD EXCERCISE

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *mul - Program multiplies two intergers
+ *
+ *@a: interger that is being used
+ *@b: interger that is being used
+ *
+ *Return: This program returns 0
+ */
+int mul(int a, int b)
+{
+int multi;
+
+	multi = a * b;
+
+	return (0);
+}
