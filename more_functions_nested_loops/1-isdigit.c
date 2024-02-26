60397eef44e71d52266a1970f6ced2fcc2ce6ace
Kryss <kryssbm99@gmail.com>
2024-02-26 10:40:39 -0400

    WEEK 4 SECOND EXCERCISE

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isdigit - Program checks for digits and letters 
+ *
+ *@c: character that is being checked
+ *
+ *Return: This program returns 1 if its digits, returns 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		{
+		return (1);
+		}
+	else if (c >= 'a' && c <= 'z')
+		{
+		return (0);
+		}
+	return (0);
+}
