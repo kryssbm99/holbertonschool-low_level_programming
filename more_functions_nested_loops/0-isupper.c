839d648ffe93372aa9cf8788d73532968ffe6da7
Kryss <kryssbm99@gmail.com>
2024-02-26 10:07:53 -0400

    WEEK 4 HERE WE GO

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isupper - Program checks for uppercase
+ *
+ *@c: character that is being checked
+ *
+ *Return: This program returns 1 if uppercase and returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		{
+		return (1);
+		}
+	else if (c >= 'a' && c <= 'z')
+		{
+		return (0);
+		}
+}
