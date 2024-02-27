d6243a323f5910e9ba19fd552840c882b2208084
Kryss <kryssbm99@gmail.com>
2024-02-27 12:38:49 -0400

    Learning the array ropes

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *reset_to_98 - set the integer to 98
+ *
+ *
+ *@n: a pointer to the integer we want to set to 98
+ *
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+	printf("Value of 'n': %p\n", n);
+	printf("Address of 'n': %p\n", &n);
+	*n = 98;
+
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Welcome to my pointers, arrays and strings basic training!
