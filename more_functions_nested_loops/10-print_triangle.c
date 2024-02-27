facd4bf0b3b365de5a56fa229eb98826377ab4c9
Kryss <kryssbm99@gmail.com>
2024-02-27 11:14:29 -0400

    TRIANGLE BETTY FIX

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_triangle - Program prints # to print a triangle 
+ *print_triangle - Program prints # to print a triangle
  *@size: variable for print_triangle
  *Return: Always returns void
  */
@@ -14,12 +14,12 @@ char space = 32;
 	{
 		for (x = 0; x < size; x++)
 		{
-			for(s = size - (x + 1); s > 0; s--)
-			{	
+			for (s = size - (x + 1); s > 0; s--)
+			{
 				_putchar(space);
 			}
 				for (y = 0; y <= x; y++)
-				{	
+				{
 					_putchar('#');
 				}
 		_putchar('\n');
