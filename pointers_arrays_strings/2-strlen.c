06c99e6d6d304799e9253ee0a7af2758615620e4
Kryss <kryssbm99@gmail.com>
2024-02-27 13:46:31 -0400

    Making strlen()

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - _strlen main function
+ *
+ *
+ *@s: a pointer to char s
+ *
+ *Return: void
+ */
+void int _strlen(char *s)
+{
+int c = 0;
+	while (*s != '\0')
+	{
+		c++;
+		p++;
+	}
+	return (c);
+}
