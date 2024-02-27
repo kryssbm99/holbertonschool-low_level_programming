81e5e44b43c9602249e219e3981c3ccf8b11518f
Kryss <kryssbm99@gmail.com>
2024-02-27 11:06:03 -0400

    TRIANGLE

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *print_triangle - Program prints # to print a triangle 
+ *@size: variable for print_triangle
+ *Return: Always returns void
+ */
+void print_triangle(int size)
+{
+int x;
+int y;
+int s;
+char space = 32;
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for(s = size - (x + 1); s > 0; s--)
+			{	
+				_putchar(space);
+			}
+				for (y = 0; y <= x; y++)
+				{	
+					_putchar('#');
+				}
+		_putchar('\n');
+
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
