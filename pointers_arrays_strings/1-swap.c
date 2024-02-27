dabc6a983ad0ffdd5375aa429d643bc1d4742790
Kryss <kryssbm99@gmail.com>
2024-02-27 13:07:13 -0400

    Swaping ints with pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - swaps int a and b
+ *
+ *@a: a pointer to the integer we want to swap
+ *@b: a pointer to the integer we want to swap
+ *
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+
+}
