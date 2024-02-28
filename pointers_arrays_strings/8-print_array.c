d391927a6b9b9356f5201c44c7f5cb4c993011d6
Kryss <kryssbm99@gmail.com>
2024-02-28 01:18:01 -0400

    Late Night Coding #8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_array - Program prints n elements of an array of intergers
+ *
+ *@a: a pointer to int a
+ *@n: integer n
+ *
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+int x = 0;
+
+	while (x < n - 1)
+	{
+		printf("%d, ", a[x]);
+		x++;
+	}
+		printf("%d\n", a[n - 1]);
+}
