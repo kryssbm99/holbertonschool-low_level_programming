3461da8884ab37dca9f8a500196793f8f1d329d1
Kryss <kryssbm99@gmail.com>
2024-02-28 02:11:39 -0400

    Final coding of the night! Proyect done

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - Program copies the string pointer to by src
+ *
+ *@dest: destination pointer
+ *@src: source pointer
+ *
+ *Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+int x;
+
+	for (x = 0; src[x]; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
