796abb3b00d66c8e7b781ce5976a38ade37e9cf8
Kryss <kryssbm99@gmail.com>
2024-03-04 23:38:01 -0400

    #0 memset FIX

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
+ *_memset - Function that fills memory with a constant byte
  *
+ *@s:Char pointer
+ *@b:Buffer address of memory
+ *@n:Unsigned int n
  *
- *
- *
- *
- *
+ *Return:Program returns s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *p = s;
+
 	while (n > 0)
 	{
 		*p = b;
