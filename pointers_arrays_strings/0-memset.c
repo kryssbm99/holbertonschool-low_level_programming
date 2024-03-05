90f240db72d5c016d9ce4fe5d412b4d85310585a
Kryss <kryssbm99@gmail.com>
2024-03-04 23:21:28 -0400

    Pushing #0 memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+	while (n > 0)
+	{
+		*p = b;
+		p++;
+		n--;
+	}
+	return (s);
+}
