65b9dbb26d1ff851c44c410f146313f3d2b74c95
Kryss <kryssbm99@gmail.com>
2024-03-13 18:06:25 -0400

    Fixing #1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,15 +11,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *result;
 	size_t len1 = 0;
 	size_t len2 = 0;
-	size_t i;
-	size_t j;
+	size_t i, j;
 
-	if (s1 == NULL)
+	if (s1 == NULL || s2 == NULL)
 	{
 		s1 = "";
-	}
-	if (s2 == NULL)
-	{
 		s2 = "";
 	}
 	while (s1[len1] != '\0')
