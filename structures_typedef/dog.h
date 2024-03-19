8791753911ac05c29e0c3e7854b794ceac247856
Kryss <kryssbm99@gmail.com>
2024-03-19 15:34:44 -0400

    10 hour coding session starts now

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *struct dog - Structure with basic information of a dog
+ *@age: float value for age of dog
+ *@name: char value for name of dog
+ *@owner: char value for name of dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
