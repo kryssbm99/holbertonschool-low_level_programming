49c6d0bc73ec525b2e628ab7ad4c82c1b9e80993
Kryss <kryssbm99@gmail.com>
2024-03-23 21:08:52 -0400

    Last fix

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,7 @@
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -5,3 +9,5 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
