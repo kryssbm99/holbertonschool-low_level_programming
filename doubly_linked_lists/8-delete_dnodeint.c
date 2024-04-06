39b2a28740eeca54fb05cac4ad09b6b36c60ff9d
Kryss <kryssbm99@gmail.com>
2024-04-06 19:38:22 -0400

    fix

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (-1);
 		}
 		index--;
-
+	}
 		if (current == *head)
 		{
 			*head  = current->next;
@@ -39,7 +39,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				current->next->prev = current->prev;
 			}
 		}
-	}
+	
 		free(current);
 		return (1);
 }
